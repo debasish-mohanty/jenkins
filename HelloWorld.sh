f85ae998c1478833200d4f7b2c225a804c60321a
debasish-mohanty <45088303+debasish-mohanty@users.noreply.github.com>
2023-06-26 23:06:09 +0530

    Update HelloWorld.sh

--- HelloWorld.sh
+++ HelloWorld.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+userhost=$(whoami)@$(hostname)
+echo "User and Host: $userhost"
+
 if ! command -v figlet &> /dev/null; then
+     echo "figlet is not installed. Installing figlet..."
      sudo apt-get install -y figlet # Assuming a Debian-based system, adjust this command for your specific package manager
 fi
 
